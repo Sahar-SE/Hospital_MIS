19e452ee920c314d57800a871c5968d3c8bd416e
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-03-12 20:42:49 +0430

    Update the controller to show the Appointments  page on the browser

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -60,7 +60,7 @@ class AppointmentsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_appointment
-      @appointment = Appointment.find(params[:id])
+      # @appointment = Appointment.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
