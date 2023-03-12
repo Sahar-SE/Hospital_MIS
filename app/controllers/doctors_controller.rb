fe3d3fb908074237f0999514167b20cb46be62f9
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-03-12 19:48:53 +0430

    Update the controller to show the Doctor page on the browser

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -60,7 +60,7 @@ class DoctorsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_doctor
-      @doctor = Doctor.find(params[:id])
+      # @doctor = Doctor.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
