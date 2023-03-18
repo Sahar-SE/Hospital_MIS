fef78906c08bde4c283b31ce215e1af5213b7869
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-03-18 18:22:35 +0430

    Add profile_image to patient model

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,5 +1,7 @@
 class Patient < ApplicationRecord
     has_one :bed
+    has_one_attached :profile_image
+    has_many_attached :documents
     # belongs_to :Appointment
     # belongs_to :doctor
 end
