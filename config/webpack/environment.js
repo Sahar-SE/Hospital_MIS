7ceed0118a61b0fbdbc414c7bf069f8b3ca28452
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-04-07 17:46:06 +0430

    Add Cofigure Bootstrap

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,4 +2,6 @@ const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack')
 
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+
 module.exports = environment
