cb09809896130485ad16e7d1cf5c4294ecb9eb0c
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-04-07 17:47:10 +0430

    Add Cofigure Bootstrap

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,5 +5,6 @@ const webpack = require('webpack')
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
 
 module.exports = environment
