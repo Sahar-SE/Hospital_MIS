3c3b7cf44d76df5a37060c6854018538e9f05c93
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-05-27 23:55:31 +0430

    Add Counts for home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,20 @@
 class HomeController < ApplicationController
+    def index
+        @doctors = Doctor.all
+        doctor_count = @doctors.count
+        @doctor_count = doctor_count
+
+        @patients = Patient.all
+        patient_count = @patients.count
+        @patient_count = patient_count
+
+        @appointments = Appointment.all
+        appointment_count = @appointments.count
+        @appointment_count = appointment_count
+
+        @beds = Bed.all
+        bed_count = @beds.count
+        @bed_count = bed_count
+
+    end
 end
